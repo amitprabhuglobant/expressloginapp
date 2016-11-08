f5377aab0b199c6e22d96c1aa1545f790d0b1ff5
amitprabhuglobant <amit.prabhu@globant.com>
2016-11-08 12:35:35 +0530

    update new changes

--- loginappexpress/routes/index.js
+++ loginappexpress/routes/index.js
@@ -2,27 +2,49 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+var users = [];
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
-// router.get('/:name', function(req, res, next) {
-//   res.json({ parameter_name: req.params.name });
-//   //res.render('index', { title: 'Express' });
+router.get('/:name', function(req, res, next) {
+    users.push({ username: req.params.name });
+    res.json(users);
+});
+
+// router.post('/:name', function(req, res, next) {
+//     users.push({ username: req.body });
+//     res.json(users);
 // });
 
-router.post('/endpoint', function(req, res){
-	var obj = {};
-  // if(req.body.user == 'test')
-  // {
-  //   res.JSON({msg:"sucess"});
-  // }
-  // else{
-  //   res.JSON({msg:"fail"});
-  // }
-	console.log('body: ' + JSON.stringify(req.body));
-	res.send(req.body);
+router.put('/:name', function(req, res, next) {
+    users.push({ username: req.body });
+    res.json(users);
 });
 
+router.delete('/:name', function(req, res, next) {
+    users.pop({ username: req.body });
+    res.json(users);
+});
+
+
+
+/* post function - requesting with userid and password*/
+router.post('/endpoint', function(req, res) {
+    var userlist = ["amitcprabhu", "amitcprabhu1", "amittest", "test", "test123"]
+
+    var result = "fail";
+    userlist.map(function(val, index, arr) {
+        if (val === req.body.user) {
+            console.log('body: user ' + req.body.user + ' is sucessfully loggedin');
+            result = "sucess";
+            return;
+        }
+    });
+
+    console.log('body: user ' + req.body.user);
+    console.log('body: password ' + req.body.pwd);
+    res.send(result);
+});
 
 module.exports = router;
--- loginappexpress/routes/users.js
+++ loginappexpress/routes/users.js
@@ -6,4 +6,40 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// router.post('/', function(req, res) {
+//   var data = req.body.newUser;
+
+//   userManage('new',data)
+// });
+
+var userManage = function () {
+  if (arguments.length < 1) {
+    return users
+  }
+  if (arguments[0] === 'new') {
+    if (arguments[1]) {
+      users.push(arguments[1]);
+      return users;
+    }
+  }
+  if (arguments[0] === 'update') {
+    if (arguments[1]) {
+      var userToUpdate = arguments[1]
+      console.log('user to update: ', userToUpdate)
+      users.forEach(function (user, index) {
+        if (user.id == userToUpdate.id) {
+          user.name = userToUpdate.name;
+          console.log('user is now: ', user)
+          users[index] = user;
+          console.log(
+            'Users is:', users
+          )
+        }
+      })
+      return users;
+    }
+  }
+}
+
+
 module.exports = router;
